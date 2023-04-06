859ee57cdea318e1767f3ea6dcb51c82bce418b6
Sagnik Mukherjee <51739091+Sagnik220@users.noreply.github.com>
2023-04-06 20:49:24 +0530

    Update main.py
    Patch Update

--- main.py
+++ main.py
@@ -1,63 +1,76 @@
+from operator import index
 import streamlit as st
-import plotly.express as px 
-import os
+import sys
+import pandas_profiling
 import pandas as pd
-import ydata_profiling as yd_profiling
 from streamlit_pandas_profiling import st_profile_report
+import os
+
+if os.path.exists('./dataset.csv'):
+    df_csv = pd.read_csv('dataset.csv', index_col=None)
 
 
-with st.sidebar: 
+with st.sidebar:
+    st.image("images\Rapidailogo.png",width=300)
     st.title("RapidAI")
-    choice=st.radio("Navigation",["Upload","Data Profiling",st.selectbox('RapidML', ['Classification', 'Regression']),"Download"])
+    choose = st.radio("Options", ['Upload', 'Data Profiling', 'Model Building', 'Download'])
     st.info("Accelerate Insights, Empower Innovation: Unleash the Power of RapidAI")
 
-if os.path.exists("dataset.csv"): 
-    df = pd.read_csv('dataset.csv', index_col=None)
+
+if choose == "Upload":
+    st.title("Upload Your CSV Dataset")
+    data_uploaded_csv = st.file_uploader("Upload Your CSV Dataset")
+    if data_uploaded_csv:
+        df_csv = pd.read_csv(data_uploaded_csv, index_col=None, encoding='ISO-8859-1')
+        df_csv.to_csv('dataset.csv', index=None)
+        st.dataframe(df_csv, width=900)
 
 
-if choice == "Upload": 
-    st.title("Upload")
-    file = st.file_uploader("Upload Dataset")
-    if file: 
-        df = pd.read_csv(file, index_col=None)
-        df.to_csv("dataset.csv", index=None)
-        st.dataframe(df)
-
-if choice == "Data Profiling": 
-    st.title("Profile")
-    profile_df = df.profile_report()
+if choose == "Data Profiling":
+    st.title("Rapid Exploratory Data Analysis")
+    profile_df = df_csv.profile_report()
+    st_profile_report(profile_df)
+    profile_df = df_csv.profile_report()
     st_profile_report(profile_df)
 
+if choose == "Model Building":
 
-if choice == "Classification":
-    target = st.selectbox("Choose the Target for Classification", df.columns)
-    if st.button("Run Classification"):
-        from pycaret.classification import setup, compare_models, pull, save_model
+    option = st.selectbox("Select your problem type",
+                          ("Select an option", "Regression", "Classification"))
+    st.write('You selected:', option)
 
-        setup(df, target=target)
-        setup_df = pull()
-        st.dataframe(setup_df)
-        best_model = compare_models()
-        compare_df = pull()
-        save_model(best_model, 'best_model')
-        st.dataframe(compare_df)
+    if option == "Classification":
+        from pycaret.classification import setup, compare_models, pull, save_model, load_model, plot_model,predict_model
 
-if choice == "Regression":
-    target = st.selectbox("Choose the Target for Regression", df.columns)
-    if st.button("Run Regression"):
-        from pycaret.regression import setup, compare_models, pull, save_model
+        options_input = st.multiselect("Choose the input features", df_csv.columns)
+        option_target = st.selectbox("Choose the target features", df_csv.columns)
+        if st.button("Train Model"):
+            X = df_csv[options_input]
+            y = df_csv[option_target]
+            setup(data=X, target=y)
+            best_model_classification = compare_models()
+            compare_df = pull(best_model_classification)
+            st.dataframe(compare_df)
+            st.info(best_model_classification)
+            save_model(best_model_classification, 'best_model')
+           
+    if option == "Regression":
+        from pycaret.regression import setup, compare_models, pull, save_model, load_model, plot_model, predict_model
 
-        setup(df, target=target)
-        setup_df = pull()
-        st.dataframe(setup_df)
-        best_model = compare_models()
-        compare_df = pull()
-        save_model(best_model, 'best_model')
-        st.dataframe(compare_df)
-    
-if choice == "Download":
-    with open("best_model.pkl", 'rb') as f: 
-        st.download_button("Download Model", f, "best_model_test.pkl")
+        options_input = st.multiselect("Choose the input features", df_csv.columns)
+        option_target = st.selectbox("Choose the target features", df_csv.columns)
+        if st.button("Train Model"):
+            X = df_csv[options_input]
+            y = df_csv[option_target]
+            setup(data=X, target=y)
+            best_model_regression = compare_models()
+            compare_df = pull(best_model_regression)
+            st.dataframe(compare_df)
+            st.info(best_model_regression)
+            save_model(best_model_regression, 'best_model')
+            
+if choose == "Download":
+    with open('best_model.pkl', 'rb') as f:
+        st.download_button('Download Model', f, file_name="best_model.pkl")
 
-
-    
+ 
